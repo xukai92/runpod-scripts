3bb79731a7931c2ba976ee1662f5e2800817cd27
Kai Xu <me@xuk.ai>
2024-11-29 21:07:56 -0500

    fix: fisher
    Signed-off-by: Kai Xu <me@xuk.ai>

--- my-start-kai.sh
+++ my-start-kai.sh
@@ -1,7 +1,9 @@
-# install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" --force
+mkdir -p $HOME/tmp
+cd $HOME/tmp
 
-# post Homebrew
+# install Homebrew
+curl -L https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh > install-brew.sh
+NONINTERACTIVE=1 bash install-brew.sh
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # install fish
@@ -11,14 +13,16 @@ brew install fish
 chsh -s /home/linuxbrew/.linuxbrew/bin/fish
 
 # install fisher
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+curl -L https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish > fisher.fish
+source fisher.fish
+fisher install jorgebucaran/fisher
 fisher install jhillyerd/plugin-git
 
 # install Mamba
 curl -LO https://github.com/conda-forge/miniforge/releases/download/24.9.2-0/Miniforge3-Linux-x86_64.sh
-bash Miniforge3-Linux-x86_64.sh -b -p /home/kai/.conda -f
-/home/kai/.conda/bin/mamba init fish
+bash Miniforge3-Linux-x86_64.sh -b -p $HOME/.conda -b
+$HOME/.conda/bin/mamba init fish
 
 # create SSH key
-ssh-keygen -t ed25519 -C "kai@runpod" -f /home/kai/.ssh/id_ed25519 -N ""
-cat /home/kai/.ssh/id_ed25519.pub
+ssh-keygen -t ed25519 -C "kai@runpod" -f $HOME/.ssh/id_ed25519 -N ""
+cat $HOME/.ssh/id_ed25519.pub
